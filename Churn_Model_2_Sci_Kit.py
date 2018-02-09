c40c0f222f9b2f43bbe0c450623a34ad180a5b99
durvesh123 <durveshvedak@172-16-42-173.DYNAPOOL.NYU.EDU>
2018-02-09 15:28:08 -0500

    Improved accuracy

--- Churn_Model_2_Sci_Kit.py
+++ Churn_Model_2_Sci_Kit.py
@@ -3,6 +3,9 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
+from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.ensemble import ExtraTreesClassifier
 
 
 #loading train data set
@@ -11,25 +14,74 @@ data = pd.read_csv('/Users/durveshvedak/Downloads/Kaggle Churn/train.csv',header
 
 data.COLLEGE = data.COLLEGE.replace({"one":int(1),"zero":int(0)})
 data.REPORTED_SATISFACTION = data.REPORTED_SATISFACTION.replace(
-                                    {"very_unsat":int(5),
-                                    "unsat":int(4),
-                                    "avg":int(3),
-                                    "sat":int(2),
-                                    "very_sat":int(1)}
+                                    {"very_unsat":int(500),
+                                    "unsat":int(400),
+                                    "avg":int(300),
+                                    "sat":int(200),
+                                    "very_sat":int(100)}
                                     )
 data.REPORTED_USAGE_LEVEL = data.REPORTED_USAGE_LEVEL.replace(
-                                    {"very_little":int(3),
-                                    "little":int(5),
-                                    "avg":int(1),
-                                    "high":int(2),
-                                    "very_high":int(4)}
+                                    {"very_little":int(500),
+                                    "little":int(400),
+                                    "avg":int(300),
+                                    "high":int(200),
+                                    "very_high":int(100)}
                                     )
 data.CONSIDERING_CHANGE_OF_PLAN = data.CONSIDERING_CHANGE_OF_PLAN.replace(
-                                    {"actively_looking_into_it":int(4),
-                                    "considering":int(5),
-                                    "perhaps":int(1),
-                                    "never_thought":int(2),
-                                    "no":int(3)}
+                                    {"actively_looking_into_it":int(500),
+                                    "considering":int(400),
+                                    "perhaps":int(300),
+                                    "never_thought":int(200),
+                                    "no":int(100)}
+
                                     )
 
-print(data.head(10))                                    
+#load test data
+data_test_all = pd.read_csv('/Users/durveshvedak/Downloads/Kaggle Churn/test.csv',header=0)
+
+data_test_all.COLLEGE = data_test_all.COLLEGE.replace({"one":int(1),"zero":int(0)})
+data_test_all.REPORTED_SATISFACTION = data_test_all.REPORTED_SATISFACTION.replace(
+                                    {"very_unsat":int(500),
+                                    "unsat":int(400),
+                                    "avg":int(300),
+                                    "sat":int(200),
+                                    "very_sat":int(100)}
+                                    )
+data_test_all.REPORTED_USAGE_LEVEL = data_test_all.REPORTED_USAGE_LEVEL.replace(
+                                    {"very_little":int(500),
+                                    "little":int(400),
+                                    "avg":int(300),
+                                    "high":int(200),
+                                    "very_high":int(100)}
+                                    )
+data_test_all.CONSIDERING_CHANGE_OF_PLAN = data_test_all.CONSIDERING_CHANGE_OF_PLAN.replace(
+                                    {"actively_looking_into_it":int(500),
+                                    "considering":int(400),
+                                    "perhaps":int(300),
+                                    "never_thought":int(200),
+                                    "no":int(100)}
+                                    )
+
+
+# Training data
+X = data.iloc[:,0:11]
+Y = data.iloc[:,11]
+
+#Normalize and Scale training set data
+X = preprocessing.normalize(X)
+X = preprocessing.scale(X)
+
+
+X_test = data_test_all.iloc[:,0:11]
+X_test = preprocessing.normalize(X_test)
+X_test = preprocessing.scale(X_test)
+
+
+#Initialize Model
+logististicModel = LogisticRegression(C=0.2)
+logististicModel.fit(X, Y)
+predictions = logististicModel.predict(X_test)
+
+
+score = logististicModel.score(X,Y)
+print(score)
